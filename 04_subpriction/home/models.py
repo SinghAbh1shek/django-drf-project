6d0c823f092f39e83f26a1ef42f17d1ae500020d
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-04-08 18:39:22 +0530

    database modeling

--- 04_subpriction/home/models.py
+++ 04_subpriction/home/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from utils.utility import BaseModel
 
-# Create your models here.
+class Blog(BaseModel):
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    is_paid = models.BooleanField(default=False)
+
+class Subscription(BaseModel):
+    title = models.CharField(max_length=100)
+    validity = models.IntegerField(default=30)
+    price = models.IntegerField()
+
+class SubscriptionOrder(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+    expiry = models.DateField()
+    is_paid = models.BooleanField(default=False)
